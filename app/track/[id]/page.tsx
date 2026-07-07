0c8661b314ec9ed423df4149028f6b1b93ff2a17
Hayk149 <Hayk.Pogosyan.03@yandex.ru>
2026-07-07 18:22:35 +0300

    страница трека без стилей

--- app/track/[id]/page.tsx
+++ app/track/[id]/page.tsx
@@ -1,10 +1,1 @@
-interface TrackPageProps {
-  params: Promise<{
-    id: string;
-  }>;
-}
-
-export default async function TrackPage({params}: TrackPageProps) {
-    const {id} = await params
-    return <h1>Страница трека c id {id}</h1>;
-}
+ 
